c5e4c75f8495e34f6a9ffb15aa94666c9545bf3b
Muzammil Dafedar <mujmildafedaar@gmail.com>
2023-12-07 11:15:34 +0530

    Initiated setup for chakra ui and developed login

--- pages/_layout.jsx
+++ pages/_layout.jsx
@@ -1,10 +1,10 @@
 import Header from '../components/Layout/AppHeader';
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function RootLayout({ children }) {
   return (
-    <div className="overflow-hidden h-screen">
-      <Header />
-      <div className="main-body">{children}</div>
-    </div>
+    <ChakraProvider>
+      <Header>{children}</Header>
+    </ChakraProvider>
   );
 }
