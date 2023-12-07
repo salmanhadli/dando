296e7b9a78e636fb73263552ce61c61027f05326
Muzammil Dafedar <mujmildafedaar@gmail.com>
2023-12-07 13:33:19 +0530

    main body css handled for login page

--- pages/_layout.jsx
+++ pages/_layout.jsx
@@ -1,12 +1,17 @@
 import Header from '../components/Layout/AppHeader';
 import { ChakraProvider } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 export default function RootLayout({ children }) {
+  const router = useRouter();
+
   return (
     <ChakraProvider>
       <div className="overflow-hidden h-screen">
         <Header />
-        <div className="main-body">{children}</div>
+        <div className={router.pathname == "/login" ? "" : " main-body"}>
+          {children}
+        </div>
       </div>
     </ChakraProvider>
   );
